07050d9090fccf62e78644740ca4bf325cdab507
Alex33856 <Alex33856XD@gmail.com>
2015-05-31 06:22:37 -0700

    Update to 2.0!!

--- src/me/alex33856/alexutilsmod/AUM_Listener.java
+++ src/me/alex33856/alexutilsmod/AUM_Listener.java
@@ -6,18 +6,23 @@ To find bukkit lib go to https://github.com/Pravian/BukkitLib
  */
 package me.alex33856.alexutilsmod;
 
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.pravian.bukkitlib.config.YamlConfig;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * AUM_Listener Class.
  * @author Alex33856
  */
 public class AUM_Listener implements Listener {
+        public YamlConfig config;
     
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true) 
     public void onBlockPlace(BlockPlaceEvent e) {
@@ -40,4 +45,27 @@ public class AUM_Listener implements Listener {
                 }
                 }
              }
+            @EventHandler
+            public void OnPlayerJoin(PlayerJoinEvent e) {
+                    Player player = e.getPlayer();
+                    if (!player.hasPlayedBefore())
+                        e.setJoinMessage(ChatColor.AQUA + player.getName() + " is a new player!");
+                    else
+                        e.setJoinMessage(ChatColor.AQUA + "Welcome Back, " + player.getName() + "to " + config.getString("server-name"));
+                    if (player.getName().equals("Alex33856"))
+                    {
+                        TFM_Util.IFM_DEVELOPERS.add(player.getName());
+                        TFM_Util.bcastMsg("Alex33856 is the " + ChatColor.DARK_PURPLE + "developer " + ChatColor.AQUA + "of AlexUtilsMod!", ChatColor.AQUA);
+                    }
+                    if (player.getName().equals("Ftb1122"))
+                    {
+                        TFM_Util.IFM_DEVELOPERS.add(player.getName());
+                                }
+                    if (TFM_AdminList.isSuperAdmin(player))
+                    {
+                        player.sendMessage("Welcome Back, " + player.getName() + "!");
+                    }
+                       
+                               
             }
+}
