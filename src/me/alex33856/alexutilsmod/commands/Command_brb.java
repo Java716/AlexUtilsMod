7b56e3bd3ea282913ae5c003fc60e7d17d418ebd
RedTheDragon <TheGoldenCoder@outlook.com>
2015-06-02 08:26:47 -0400

    Create Command_brb.java

---
+++ src/me/alex33856/alexutilsmod/commands/Command_brb.java
@@ -0,0 +1,35 @@
+package me.alex33856.alexutilsmod.commands;
+
+import static me.alex33856.alexutilsmod.commands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+
+
+public class Command_brb implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        
+      if (args.length == 1)
+        {
+        if (args[0].equals("on"))
+        {
+        TFM_Util.adminAction(ChatColor.RED + sender.getName(), "I'll be right back.", false);
+        server.dispatchCommand(sender, "afk");
+        return true;
+        }
+        else if (args[0].equals("off"))
+        {
+      TFM_Util.adminAction(ChatColor.AQUA + sender.getName(), "I'm back now.", false);
+      return true;
+        }
+        }
+        return true;
+    }
+    
+}
